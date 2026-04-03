2ba10de9893eaa11814d30106b3346e5f02a2a19
Ingo Paschke <ipaschke@lpclabs.de>
2026-04-03 14:36:50 +0200

    Add build and packaging tools for Mac 68k port
    Python scripts for resource fork manipulation, disk image creation,
    and BinHex decoding. Rez source for SIZE resource. Retro68 patch.

---
+++ sys/mac/nhsize.r
@@ -0,0 +1,6 @@
+/* SIZE resource for NetHack */
+data 'SIZE' (-1) {
+    $"5880"             /* flags: multiFinderAware, canBackground, is32BitCompatible */
+    $"00800000"         /* preferred size = 8388608 = 8MB */
+    $"00400000"         /* minimum size = 4194304 = 4MB */
+};
