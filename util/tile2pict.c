fcbbc4fc264309ca4abf210f062f4d2e8e4e80e9
Ingo Paschke <ipaschke@lpclabs.de>
2026-05-06 20:01:40 +0200

    tile2pict: count tiles in stock txt input

--- util/tile2pict.c
+++ util/tile2pict.c
@@ -15,16 +15,34 @@ static const char *const tilefilenames[3] = {
     "monsters.txt", "objects.txt", "other.txt"
 };
 
+#define TILES_PER_ROW 30   /* 30 * 16 = 480 px wide sheet */
+
+static pixel tilepixels[TILE_Y][TILE_X];
+
 int
 main(int argc, char *argv[])
 {
+    int total_tiles = 0, i;
+    char path[256];
+
     if (argc != 2) {
         fprintf(stderr, "usage: %s outfile.r\n", argv[0]);
         return 1;
     }
     objects_globals_init();
     monst_globals_init();
-    /* Real work in later tasks. */
-    fprintf(stderr, "tile2pict: skeleton (writes nothing yet)\n");
+
+    /* Pass 1: count tiles. */
+    for (i = 0; i < 3; ++i) {
+        snprintf(path, sizeof path, "%s%s", relative_tiledir, tilefilenames[i]);
+        if (!fopen_text_file(path, RDTMODE)) {
+            fprintf(stderr, "cannot open %s\n", path);
+            return 1;
+        }
+        while (read_text_tile(tilepixels))
+            ++total_tiles;
+        fclose_text_file();
+    }
+    fprintf(stderr, "tile2pict: %d tiles total\n", total_tiles);
     return 0;
 }
